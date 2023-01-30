4c2711e0af69672b74a09431d1f9fb16360a75a5
Denis Dmitriev <der.flinger1397@gmail.com>
2023-01-30 10:35:24 +0300

    enhanced EXample_3

---
+++ Example003_/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+
+Console.Write("Enter first number: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write($"The sum of number = {numberA + numberB}");
