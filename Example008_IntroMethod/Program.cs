04338220d77a89c48868f64f5ca6393ff9eae905
Denis Dmitriev <der.flinger1397@gmail.com>
2023-01-30 12:22:43 +0300

    two task was solved

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+
+int Max(int num1, int num2, int num3)
+{
+    int max = num1;
+    if (num2 > max) max = num2;
+    if (num3 >  max) max = num3;
+    return max;
+}
+// int n1 = int.Parse(Console.ReadLine());
+// int n2 = int.Parse(Console.ReadLine());
+// int n3 = int.Parse(Console.ReadLine());
+
+// int largest = Max(n1, n2, n3);
+
+Console.WriteLine(Max(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = {14,53,87,44,12,978,54,223,654};
+
+int Max(int num1, int num2, int num3)
+{
+    int max = num1;
+    if (num2 > max) max = num2;
+    if (num3 >  max) max = num3;
+    return max;
+}
+
+int max1 = Max(array[0], array[1], array[2]);
+int max2 = Max(array[3], array[4], array[5]);
+int max3 = Max(array[6], array[7], array[8]);
+
+Console.WriteLine(Max(max1, max2, max3));
